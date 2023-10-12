3ad0a5c22940c2cdae85df44baaf73ea78aaf997
Gambhiro <gambhiro@users.noreply.github.com>
2023-10-12 17:33:03 +0100

    emacs instructions and code highlighting

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -19,10 +19,6 @@
       <li><a href="/calendars/fs-year-planner-grid.pdf">fs-year-planner-grid.pdf</a></li>
       <li><a href="/calendars/fs-year-planner-portrait.pdf">fs-year-planner-portrait.pdf</a></li>
       <li><a href="/calendars/fs-year-planner-rows.pdf">fs-year-planner-rows.pdf</a></li>
-      <li><a href="/calendars/holiday-moondays.el">holiday-moondays.el</a></li>
-      <li><a href="/calendars/mahanikaya.csv">mahanikaya.csv</a></li>
-      <li><a href="/calendars/mahanikaya.xlsx">mahanikaya.xlsx</a></li>
-      <li><a href="/ical/mahanikaya.ical">mahanikaya.ical</a></li>
     </ul>
 
     <p>CSV, XLSX</p>
@@ -36,13 +32,64 @@
 
     <ul>
       <li><a href="/ical/mahanikaya.ical">mahanikaya.ical</a></li>
-      <li><a href="/calendars/holiday-moondays.el">holiday-moondays.el</a></li>
     </ul>
+
+    <p>
+      Copy the .ical url to import in your calendar application, or add to Google Calendar.
+    </p>
+
+    <p>
+      <img src="~/assets/images/add-by-url.png" style="border: 1px solid rgb(196, 196, 196); padding: 5px;" title="Add By Url">
+    </p>
 
     <p>Emacs</p>
 
     <ul>
       <li><a href="/calendars/holiday-moondays.el">holiday-moondays.el</a></li>
     </ul>
+
+    <p>
+      This defines calendar events to show with <code>M-x calendar</code> in emacs. Place it in the folder where you load local packages from.
+    </p>
+
+    <pre class="has-code">
+      <code class="language-lisp">
+(add-to-list 'load-path "~/path/to/local/packages")
+(require 'holiday-moondays)
+      </code>
+    </pre>
+
+    <p>
+      <img src="~/assets/images/emacs-calendar-moondays.png">
+    </p>
+
+    <p>
+      The complete list from 2000-2100 is quite long, so it's probably a good idea to filter it for the relevant years. Download with <code>wget</code> and filter with <code>sed</code> for example:
+    </p>
+
+    <pre class="has-code">
+      <code class="language-shell">
+wget -O - "https://splendidmoons.github.io/calendars/holiday-moondays.el" |\
+    sed '/^(holiday-sexp/ { /\(2022\|2023\|2024\|2025\)/ !d; };' > holiday-moondays.el
+      </code>
+    </pre>
+
   </div>
 </template>
+
+<script>
+ import hljs from 'highlight.js/lib/core';
+ import lisp from 'highlight.js/lib/languages/lisp';
+ import shell from 'highlight.js/lib/languages/shell';
+
+ hljs.registerLanguage('lisp', lisp);
+ hljs.registerLanguage('shell', shell);
+
+ export default {
+   mounted() {
+     document.querySelectorAll('pre code').forEach(el => {
+       hljs.highlightElement(el);
+     });
+   }
+ }
+</script>
--- pages/import-in-python.vue
+++ pages/import-in-python.vue
@@ -4,26 +4,47 @@
     <p>
       Install the <a href="https://github.com/splendidmoons/splendidmoons">splendidmoons</a> package:
     </p>
-    <p>
-      <pre>
+
+    <pre class="has-code">
+      <code class="language-shell">
 $ pip install splendidmoons
 $ splendidmoons asalha-puja 2023
 2023-08-01
 $ splendidmoons year-events-csv 2020 2030 moondays.csv
-      </pre>
-    </p>
+      </code>
+    </pre>
+
     <p>
       In Python scripts:
     </p>
-    <p>
-      <pre>
+
+    <pre class="has-code">
+      <code class="language-python">
 from splendidmoons.calendar_year import CalendarYear
 for year in [2023, 2024, 2025]:
     print(f"{year}: {CalendarYear(year).year_type()}")
 # 2023: YearType.Adhikamasa
 # 2024: YearType.Normal
 # 2025: YearType.Adhikavara
-      </pre>
-    </p>
+      </code>
+    </pre>
+
   </div>
 </template>
+
+<script>
+ import hljs from 'highlight.js/lib/core';
+ import python from 'highlight.js/lib/languages/python';
+ import shell from 'highlight.js/lib/languages/shell';
+
+ hljs.registerLanguage('python', python);
+ hljs.registerLanguage('shell', shell);
+
+ export default {
+   mounted() {
+     document.querySelectorAll('pre code').forEach(el => {
+       hljs.highlightElement(el);
+     });
+   }
+ }
+</script>
